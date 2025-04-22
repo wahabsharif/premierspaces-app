3f497154d9315e69ef5588b34f86f1607f55a5f8
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-04-22 23:17:11 +0500

    api updated

--- Constants/env.ts
+++ Constants/env.ts
@@ -1,2 +1,2 @@
-// export const baseApiUrl = "http://easyhomz.co.uk/mapp";
-export const baseApiUrl = "http://192.168.18.106:8000/api/mapp";
+export const baseApiUrl = "http://easyhomz.co.uk/mapp";
+// export const baseApiUrl = "http://192.168.1.16:8000/api/mapp";
